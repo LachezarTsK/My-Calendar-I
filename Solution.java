7d89c2bc9501cac2e97a229d076719f955356b62
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-08-07 11:11:15 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -4,20 +4,17 @@ import java.util.TreeSet;
 public class MyCalendar {
 
     private record Interval(int start, int end) {}
-    private final TreeSet<Interval> booking;
+    private final TreeSet<Interval> bookedMeetings;
 
     public MyCalendar() {
-        booking = new TreeSet<>((first, second) -> first.start - second.start);
+        bookedMeetings = new TreeSet<>((first, second) -> first.start - second.start);
     }
 
     public boolean book(int start, int end) {
         Interval interval = new Interval(start, end);
 
-        if ((booking.ceiling(interval) == null || booking.ceiling(interval).start >= interval.end)
-           && (booking.floor(interval) == null || booking.floor(interval).end <= interval.start)) {
-
-            return booking.add(interval);
-        }
-        return false;
+        return (bookedMeetings.ceiling(interval) == null || bookedMeetings.ceiling(interval).start >= interval.end)
+                && (bookedMeetings.floor(interval) == null || bookedMeetings.floor(interval).end <= interval.start)
+                && bookedMeetings.add(interval);
     }
 }

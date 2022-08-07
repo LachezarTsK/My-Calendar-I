41d7e38f6056e89b9bd0217a8a6f115516444e8b
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-08-07 08:17:47 +0200

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,24 @@
+
+#include <set>
+using namespace std;
+
+class MyCalendar {
+
+    struct Interval {
+        int start{};
+        int end{};
+        Interval(int start, int end) : start{start}, end{end}{}
+    };
+
+    struct CompareIntervals {
+        bool operator()(const Interval& x, const Interval& y) const {return x.end <= y.start;};
+    };
+    set<Interval, CompareIntervals> bookedMeetings;
+
+public:
+    MyCalendar() = default;
+
+    bool book(int start, int end) {
+        return bookedMeetings.emplace(Interval(start, end)).second;
+    }
+};
